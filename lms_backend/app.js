c0e7a1a89a5634bc2098a1952660e5be7627506e
Vatsal0313 <117883036+Vatsal0313@users.noreply.github.com>
2025-04-30 17:37:46 +0530

    Update app.js

--- lms_backend/app.js
+++ lms_backend/app.js
@@ -37,6 +37,19 @@ app.options("*", cors({
   origin: "https://leadmanagementsystem.vercel.app",
   credentials: true
 }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://leadmanagementsystem.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // 👈 important
+  }
+
+  next();
+});
+
 //Changes end
 app.use(cookieParser());
 
