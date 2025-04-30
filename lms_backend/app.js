0e704f9ab127f11317c2aaaaab2b6630e29cb93c
Vatsal0313 <117883036+Vatsal0313@users.noreply.github.com>
2025-04-30 17:16:29 +0530

    Update app.js

--- lms_backend/app.js
+++ lms_backend/app.js
@@ -34,15 +34,6 @@ app.options("*", cors({
   credentials: true
 }));
 
-res.setHeader("Access-Control-Allow-Credentials", "true");
-res.setHeader("Access-Control-Allow-Origin", "https://leadmanagementsystem.vercel.app");
-res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization");
-
-if (req.method === 'OPTIONS') {
-  res.status(200).end();
-  return;
-}
 //Changes end
 app.use(cookieParser());
 
